e779e29b83d76fc88b9f5feea7ac73ba4325a4b2
CGX <2271161997@qq.com>
2019-12-08 14:45:44 +0800

    minyao project is first comminted

---
+++ src/main/java/com/cduestc/minyao/MinYaoApplication.java
@@ -0,0 +1,11 @@
+package com.cduestc.minyao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinYaoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MinYaoApplication.class, args);
+    }
+}
